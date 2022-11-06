8238b702856ac34e8a0706e7b8e6fc3d057c7b90
DominykMcNamara <dominyksmith@gmail.com>
2022-11-05 20:16:52 -0400

    added routes for job listing api

---
+++ pages/api/jobs/[id].js
@@ -0,0 +1,18 @@
+import jobsData from "../../../data.json";
+
+const handler = async ({ query: { id } }, res) => {
+  try {
+    const jobListing = jobsData.filter((job) => (job.id = id));
+    if (jobListing) {
+      res.status(200).json(jobListing[0]);
+    } else {
+      res
+        .status(404)
+        .json({ message: `Job listing with an id of ${id} cannot be found.` });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default handler;
---
+++ pages/api/jobs/index.js
@@ -0,0 +1,16 @@
+import jobsData from '../../../data.json'
+
+const handler = async (req, res) => {
+    try {
+       if (jobsData) {
+        res.status(200).json(jobsData)
+        console.log(jobsData)
+       } else {
+        res.status(404).json({ message: 'Cannot find any job listings'})
+       }
+    } catch (err){
+        console.log(err)
+    }
+}
+
+export default handler
