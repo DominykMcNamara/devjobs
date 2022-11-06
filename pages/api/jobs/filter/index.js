a3f6a563d480233cc6864befe7f53a24d904487e
DominykMcNamara <dominyksmith@gmail.com>
2022-11-05 20:53:00 -0400

    refractor filter route

---
+++ pages/api/jobs/filter/index.js
@@ -0,0 +1,11 @@
+import jobsData from '../../../../data.json'
+
+const handler = async ({ query: {title, location, fullTime}}, res) => {
+    try {
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export default handler
