b27029fec0a2287239882e1c609715272f2d7b0f
DominykMcNamara <dominyksmith@gmail.com>
2022-11-05 20:45:39 -0400

    filter by location route

--- pages/api/jobs/filter/location/[location].js
+++ pages/api/jobs/filter/location/[location].js
@@ -3,7 +3,15 @@ import jobsData from '../../../../../data.json'
 const handler = async ({ query: { location }}, res) => {
     try {
         const jobListing = jobsData.filter(job => job.location === location)
+        if (jobListing) {
+            res.status(200).json(jobListing)
+            console.log(location)
+        } else {
+            res.status(404).json({ message: `There are currently no job listings for jobs located in ${ location }`})
+        }
     } catch (err){
         console.log(err)
     }
 }
+
+export default handler
