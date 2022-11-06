f52ce48941fc6a2d0b350e521ae70409551ee4bb
DominykMcNamara <dominyksmith@gmail.com>
2022-11-05 20:29:13 -0400

    Created route to filter jobs by contract type

---
+++ pages/api/jobs/filter/fulltime.js
@@ -0,0 +1,21 @@
+import jobsData from "../../../../data.json";
+
+const handler = async (req, res) => {
+  try {
+    const fullTimeListings = jobsData.filter(
+      (job) => job.contract === "Full Time"
+    );
+    if (fullTimeListings) {
+      res.status(200).json(fullTimeListings);
+      console.log(fullTimeListings);
+    } else {
+      res
+        .status(404)
+        .json({ message: "No full time job listings can be found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default handler;
