c3c05d272e45fac0bdccbc0d5ee2628300ce5f12
DominykMcNamara <dominyksmith@gmail.com>
2022-11-06 19:50:20 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <Layout>
       <>
-      <h1>Hello world</h1>
+      <h1 className="text-h1">Hello world</h1>
       </>
     </Layout>
   )
