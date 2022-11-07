e8af0f97927a6b5e573eb3dd99053e64ea0e7448
DominykMcNamara <dominyksmith@gmail.com>
2022-11-07 16:02:55 -0500

    Completed themetoggler & added to it to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 import bg from "../assets/desktop/bg-pattern-header.svg";
+import { ThemeToggler } from "./ThemeToggler";
 
 export default function Header() {
   return (
@@ -11,8 +12,9 @@ export default function Header() {
       }}
       className="w-screen h-[10.125rem]"
     >
-    <div className="flex mx-auto w-[77.813rem] pt-[2.813rem]">
+    <div className="flex mx-auto w-[77.813rem] pt-[2.813rem] justify-around">
       <h1 className="text-h1 text-secondary-white"> devjobs </h1>
+      <ThemeToggler />
     </div>
     </div>
   );
--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import Image from "next/image";
-import sunIcon from '../assets/desktop/icon-sun.svg'
-import moonIcon from '../assets/desktop/icon-moon.svg'
+import sunIcon from "../assets/desktop/icon-sun.svg";
+import moonIcon from "../assets/desktop/icon-moon.svg";
 
 export function ThemeToggler() {
-
+  const [darkMode, setDarkMode] = useState(true);
+  const toggleTheme = () => {
+    setDarkMode(!darkMode)
+  }
+  return (
+    <div className="flex flex-row w-[7rem] justify-between align-center p-2">
+      <Image
+        src={sunIcon}
+        alt="Icon in the shape of a sun"
+        height={25}
+        width={25}
+        className="mr-1"
+      />
+      <div className="w-[3rem] h-[1.5rem] bg-secondary-white rounded-lg p-1 flex align-center">
+        <button onClick={toggleTheme} className={`cursor-pointer rounded-full bg-[#5964E0] ${darkMode ? 'focus:translate-x-5' : 'focus:-translate-x-0'}  h-[0.875rem] transition-all duration-75 w-[0.875rem]`}></button>
+      </div>
+      <Image
+        src={moonIcon}
+        alt="Icon in the shape of a moon"
+        height={25}
+        width={25}
+        className="ml-1"
+      />
+    </div>
+  );
 }
