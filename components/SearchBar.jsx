c89a2504ff5cdb171a21845df12d5eb74517152e
DominykMcNamara <dominyksmith@gmail.com>
2022-11-07 19:15:54 -0500

    completed searchbar ui

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -6,7 +6,7 @@ import SearchIcon from "../assets/desktop/icon-search.svg"
 import LocationIcon from "../assets/desktop/icon-location.svg"
 export default function SearchBar() {
   return (
-    <div className=" mx-auto flex flex-row justify-center w-[69.375rem] h-[5rem] rounded  p-2 text-p ">
+    <div className=" mx-auto flex flex-row justify-center w-screen h-[5rem] align-center  p-2 text-p ">
       <Formik
         initialValues={{
           companyOrSearchTerm: "",
@@ -15,7 +15,7 @@ export default function SearchBar() {
         }}
       >
         <Form>
-          <div className="divide-x divide-neutral-100/25 relative bottom-[2rem] flex flex-row bg-secondary-white ">
+          <div className="divide-x relative bottom-[2rem] flex flex-row bg-secondary-white rounded ">
             <label className="p-3">
             <Image src={SearchIcon} height={24} width={24} alt="Icon shaped like a magnifying glass" className="absolute top-4 ml-2" />
             <TextInput
@@ -27,19 +27,19 @@ export default function SearchBar() {
            
             </label>
             <label className="p-3">
-          <Image src={LocationIcon} height={24} width={24} alt="Icon shaped like a magnifying glass" className="absolute " />
+          <Image src={LocationIcon} height={24} width={24} alt="Icon shaped like a magnifying glass" className="absolute top-4 ml-2 " />
             <TextInput
-              className=" focus:outline-0 p-3"
+              className=" focus:outline-0 p-3 ml-10"
               name="location"
               placeholder="location"
               type="text"
             />
             </label>
 
-            <label className="p-3 font-bold text-p text-[#19202D] ">
+            <label className="p-4 font-bold text-p text-[#19202D] ">
               <Field type="checkbox" name="fullTime" className="" />
                 Full Time Only
-              <button className="ml-2 h-[3rem] w-[7.688rem] bg-[#5964E0] rounded text-secondary-white font-bold text-p text-center" type="submit">
+              <button className="ml-4 h-[3rem] w-[7.688rem] bg-[#5964E0] rounded text-secondary-white font-bold text-p text-center" type="submit">
                 Search
               </button>
             </label>
